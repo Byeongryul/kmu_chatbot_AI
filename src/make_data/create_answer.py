752a16c571eeabb39826263502ff5142d782ae36
Byeongryul <mapia56@naver.com>
2021-05-07 20:44:12 +0900

    정리

--- src/make_data/create_answer.py
+++ src/make_data/create_answer.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 QnA = pd.read_csv('rsc/data_mapping/QnA.csv').drop('Unnamed: 0', axis = 1).fillna('')
 QnA = QnA.rename(columns={'장학종류':'title','장학혜택(금액)':'장학혜택'})
-QnA['title'] = QnA['title'].apply(lambda x:x.replace(" ", ''))
-
 answer_button = pd.read_csv('rsc/sql/answer_button.csv').drop('Unnamed: 0', axis = 1).fillna('')
+
 titles = []
 documents = []
 urls = []
+
 title = ''
 for row in QnA.T:
     for col in QnA:
@@ -26,15 +26,13 @@ for row in QnA.T:
         urls.append(url)
 
 data = {'title':titles, 'document':documents, 'url':urls}
-total = pd.DataFrame(data, columns = ['title', 'document', 'url'])
-total.to_csv('rsc/sql/answer.csv')
+answer = pd.DataFrame(data, columns = ['title', 'document', 'url'])
+answer.to_csv('rsc/sql/answer.csv')
 
-inner = pd.merge(answer_button, total['url'], left_on='button_url', right_on='url', how='inner').drop('url', axis=1)
-outer = pd.merge(answer_button, total['url'], left_on='button_url', right_on='url', how='outer')
+inner = pd.merge(answer_button, answer['url'], left_on='button_url', right_on='url', how='inner').drop('url', axis=1)
+outer = pd.merge(answer_button, answer['url'], left_on='button_url', right_on='url', how='outer')
 left = outer[outer['url'].isna()].drop('url', axis=1)
 outside = left[left['button_name'].str.contains('사이트로 이동')]
-print(outside)
 outside['button_name'] = '사이트로 이동'
-print(outside)
-inner = pd.concat([inner, outside])
-inner.to_csv('rsc/sql/answer_button.csv')
+answer_button = pd.concat([inner, outside])
+answer_button.to_csv('rsc/sql/answer_button.csv')
